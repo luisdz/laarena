593200d7365825f5873d0c10cda0fe0b4ef38851
edhack7 <jeduardoh.c@gmail.com>
2019-02-01 15:01:33 -0600

    Update class.php
    actualizacion de funcion ingresar registro cliente

--- production/clases/class.php
+++ production/clases/class.php
@@ -113,160 +113,27 @@ public function consultar_renovaciones()
 
 
 
-public function consultar_topconsumos_mactual()
+
+public function ingresar_clientes()
     {
-     echo 'funcion';
-    /*
-    $srpt ="SELECT 
-            Round(sum([CTA_PRINCIPALDELTA_SIVA]), 2, 0) SIVA,
-              case 
-               when tipocdr=1 then 'VOZ' 
-               when tipocdr=2 then 'SMS'
-               when tipocdr=23 then 'Navegacion' 
-               when tipocdr=14 then 'VAS' 
-              END CATEGORIA
-             FROM [SEG_PREPAGO].[dbo].[CONSUMOS_ODS_DIA_SMRY] where 
-             [IDPERIODO]=left(convert(varchar,getdate()-1,112),6)  
-            group by tipocdr order by  Round(sum([CTA_PRINCIPALDELTA_SIVA]), 2, 0)  desc ";
+      
+      $nombre=$_POST['nombre'];
+      $apellido=$_POST['apellido'];
+      $telefono=$_POST['telefono'];
+     
+    $srpt ="INSERT INTO persona (nombre, apellido, telefono)
+VALUES ('".$nombre."', '".$apellido."', '".$telefono."')";
    
-    $qsrp = sqlsrv_query($this->conectar(),$srpt, array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
-    //echo $srpt;
-    $er = sqlsrv_num_rows($qsrp);
-    if(sqlsrv_num_rows($qsrp)==0){
-    echo 'Sin resultados';
-    }
-    else{
-      while ($rowrp = sqlsrv_fetch_array($qsrp, SQLSRV_FETCH_ASSOC)) {
-              //print_r($rowrp); 
-              $this->consumostop[] = $rowrp;
-      }
+    $qsrp = mysqli_query($this->conectar(),$srpt);
+   // echo $srpt;
+    
 
-         return $this->consumostop;
-    } */
-  }
-
-
-
-public function consultar_consumos_dia_mactual(){
          
-    $srpt ="SELECT 
-            Round(sum([CTA_PRINCIPALDELTA_SIVA]), 2, 0) SIVA,
-              case 
-               when tipocdr=1 then 'VOZ' 
-               when tipocdr=2 then 'SMS'
-               when tipocdr=23 then 'Navegacion' 
-               when tipocdr=14 then 'VAS' 
-               when tipocdr=6 then 'Aprop.'
-              END CATEGORIA
-             FROM [SEG_PREPAGO].[dbo].[CONSUMOS_ODS_DIA_SMRY] where 
-             [IDPERIODO]=left(convert(varchar,getdate()-1,112),6)  
-            group by tipocdr order by tipocdr desc ";
-   
-    $qsrp = sqlsrv_query($this->conectar(),$srpt, array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
-    //echo $srpt;
-    $er = sqlsrv_num_rows($qsrp);
-    if(sqlsrv_num_rows($qsrp)==0){
-    echo 'Sin resultados';
-    }
-    else{
-      while ($rowrp = sqlsrv_fetch_array($qsrp, SQLSRV_FETCH_ASSOC)) {
-              //print_r($rowrp); 
-              $this->consumos[] = $rowrp;
-      }
+  
 
-         return $this->consumos;
-    }
   }
 
-  public function consultar_consumos_dia_manterior(){
-      
-    $srpt ="SELECT 
-             Round(sum([CTA_PRINCIPALDELTA_SIVA]), 2, 0) SIVA,
-              case 
-               when tipocdr=1 then 'VOZ' 
-               when tipocdr=2 then 'SMS'
-               when tipocdr=23 then 'Navegacion' 
-               when tipocdr=14 then 'VAS' 
-              END CATEGORIA
-             FROM [SEG_PREPAGO].[dbo].[CONSUMOS_ODS_DIA_SMRY] where 
-             [IDPERIODO]=convert(char(6), dateadd(MM, -1, getdate()-1), 112)   and dia between '01' and  (RIGHT(convert(varchar,getdate()-1,112),2)) 
-            group by tipocdr order by tipocdr desc";
-   
-    $qsrp = sqlsrv_query($this->conectar(),$srpt, array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
-    //echo $srpt;
-    $er = sqlsrv_num_rows($qsrp);
-    if(sqlsrv_num_rows($qsrp)==0){
-    echo 'Sin resultados';
-    }
-    else{
-      while ($rowrp = sqlsrv_fetch_array($qsrp, SQLSRV_FETCH_ASSOC)) {
-              //print_r($rowrp); 
-              $this->consumos_manterior[] = $rowrp;
-      }
-
-         return $this->consumos_manterior;
-    }
-  }
-
-
-
-  public function consultar_consumos_lastm(){
-    $srpt =" SELECT 
-             [IDPERIODO],Round(sum([CTA_PRINCIPALDELTA_SIVA]), 2, 0) SIVA
-             FROM [SEG_PREPAGO].[dbo].[CONSUMOS_ODS_DIA_SMRY] where 
-             [IDPERIODO] in (convert(char(6), dateadd(MM, -5, getdate()-1), 112),convert(char(6), dateadd(MM, -4, getdate()-1), 112),convert(char(6), dateadd(MM, -3, getdate()-1), 112),convert(char(6), dateadd(MM, -2, getdate()-1), 112),convert(char(6), dateadd(MM, -1, getdate()-1), 112) ,left(convert(varchar,getdate()-1,112),6) ) 
-              and dia between '01' and  (RIGHT(convert(varchar,getdate()-1,112),2)) 
-            
-           group by [IDPERIODO]  order by [IDPERIODO] asc";
-   
-    $qsrp = sqlsrv_query($this->conectar(),$srpt, array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
-    //echo $srpt;
-    $er = sqlsrv_num_rows($qsrp);
-    if(sqlsrv_num_rows($qsrp)==0){
-    echo 'Sin resultados';
-    }
-    else{
-      while ($rowrp = sqlsrv_fetch_array($qsrp, SQLSRV_FETCH_ASSOC)) {
-              //print_r($rowrp); 
-              $this->consumos_lastm[] = $rowrp;
-      }
-
-         return $this->consumos_lastm;
-    }
-  }
-
-  public function consumos_cdr_lastm($cdr){ 
-      
-    $srpt =" 
-
-SELECT    [IDPERIODO],case 
-               when tipocdr=1 then 'VOZ' 
-               when tipocdr=2 then 'SMS'
-               when tipocdr=23 then 'Navegacion' 
-               when tipocdr=14 then 'VAS' 
-              END CATEGORIA,
-           Round(sum([CTA_PRINCIPALDELTA_SIVA]), 2, 0) SIVA
-        FROM [SEG_PREPAGO].[dbo].[CONSUMOS_ODS_DIA_SMRY]
-        where [IDPERIODO] in (convert(char(6), dateadd(MM, -4, getdate()-1), 112),convert(char(6), dateadd(MM, -3, getdate()-1), 112),convert(char(6), dateadd(MM, -2, getdate()-1), 112),convert(char(6), dateadd(MM, -1, getdate()-1), 112) ,left(convert(varchar,getdate()-1,112),6) ) 
-        and tipocdr=".$cdr." and dia between '01' and  (RIGHT(convert(varchar,getdate()-1,112),2)) 
-        group by [IDPERIODO],tipocdr  order by [IDPERIODO], tipocdr asc";
-   
-    $qsrp = sqlsrv_query($this->conectar(),$srpt, array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
-    //echo $srpt;
-    $er = sqlsrv_num_rows($qsrp);
-    if(sqlsrv_num_rows($qsrp)==0){
-    echo 'Sin resultados cdr last';
-    }
-    else{
-      while ($rowrp = sqlsrv_fetch_array($qsrp, SQLSRV_FETCH_ASSOC)) {
-              //print_r($rowrp); 
-              $consumoscdr_lastm[] = $rowrp;
-      }
-
-         return $consumoscdr_lastm;
-    }
-  }
-
+  
   
 
 
